c0c5f4899639faf80a589c8de3ad63f81746417a
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-01-27 09:44:43 -0500

    Create insert() method to add data to tree. Add notes about how new nodes are added and potential bugs to watch for

--- script.js
+++ script.js
@@ -29,13 +29,27 @@ class Node {
 }
 
 class Tree {
-  // Removed start and end from constructor as the end of the array was being calculated before the arr was sorted and duplicates removed. This caused undefined values at the end of the array to be placed in the tree
   constructor(arr) {
+    // NOTE: this.data as a variable name is duplicated from Node, may cause an issue, but leaving for now
+
+    // NOTE: arr is sorted/filtered when tree class is first created, however, there is nothing to prevent duplicates from being added to the tree later
+
     this.data = this.modArr(arr);
     this.start = 0;
     this.end = this.data.length - 1;
     this.root = this.buildTree(this.data, this.start, this.end);
   }
+
+  modArr = (arr) => {
+    const modArr = arr
+      .sort((a, b) => {
+        return a - b;
+      })
+      .filter((el, index, a) => {
+        return index === a.indexOf(el);
+      });
+    return modArr;
+  };
 
   buildTree(arr, start, end) {
     // base case
@@ -53,16 +67,20 @@ class Tree {
     return treeNode;
   }
 
-  modArr = (arr) => {
-    const modArr = arr
-      .sort((a, b) => {
-        return a - b;
-      })
-      .filter((el, index, a) => {
-        return index === a.indexOf(el);
-      });
-    return modArr;
-  };
+  // NOTE: All new values are inserted as leaf nodes, not branches
+
+  // NOTE: New insertions can cause the tree to become unbalanced - may require rebalancing
+
+  insert(treeNode, key) {
+    if (treeNode === null) return new Node(key);
+
+    if (key < treeNode.data) {
+      treeNode.left = this.insert(treeNode.left, key);
+    } else {
+      treeNode.right = this.insert(treeNode.right, key);
+    }
+    return treeNode;
+  }
 }
 
 const basic = new Tree(basicArr);
@@ -73,4 +91,8 @@ console.log(prettyPrint(basic.root));
 const regTree = new Tree(arr);
 console.log(regTree);
 console.log(regTree.root);
+regTree.insert(regTree.root, 666);
+regTree.insert(regTree.root, 9);
+regTree.insert(regTree.root, 6);
+regTree.insert(regTree.root, 10);
 console.log(prettyPrint(regTree.root));
